47b1103d75b1e6579a0f2212c26b37e482ddd40a
gold <goldhuang24@gmail.com>
2023-03-12 15:46:05 +0800

    start docker-server-client-with-database successfully for me

---
+++ simple-server-with-database/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/server/MySQLConfiguration.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.allune.quickfixj.spring.boot.starter.examples.server;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Profile("docker")
+@Configuration
+public class MySQLConfiguration {
+
+	@Bean
+	@Primary
+	public DataSource dataSource() {
+		final BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://mysqldb-server:3306/quickfix");
+		dataSource.setUsername("testuser");
+		dataSource.setPassword("testuser");
+		return dataSource;
+	}
+}
